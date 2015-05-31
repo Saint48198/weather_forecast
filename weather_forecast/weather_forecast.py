0e14a89258c3bb579879e3b33c86d7b050d6c134
sdaniels <daniels.scott.r@gmail.com>
2015-05-31 12:18:57 -0400

    Removed needed main.py and created function to generate the html and populate it with forecast data

--- weather_forecast/weather_forecast.py
+++ weather_forecast/weather_forecast.py
@@ -21,13 +21,15 @@ class WeatherForecast:
         geolocator = Nominatim()
         geo_location = geolocator.geocode(self.location)
 
+        self.location = geo_location.address
+
         # get longitude and latitude
         self.latitude = geo_location.latitude
         self.longitude = geo_location.longitude
 
     def get_date_list(self):
         # build the date list
-        start_day = datetime.datetime.today() + datetime.timedelta(days=1)
+        start_day = datetime.datetime.today() #+ datetime.timedelta(days=1)
         date_list = [start_day + datetime.timedelta(days=x) for x in range(0, 5)]
 
         return date_list
@@ -45,10 +47,11 @@ class WeatherForecast:
             parse_json = json.loads(response_data)
             weather = parse_json['list']
 
-            WeatherForecast.print_weather_forecast(self, weather)
+            WeatherForecast.print_to_console(self, weather)
+            WeatherForecast.print_to_html(self, weather)
 
 
-    def print_weather_forecast(self, forecast):
+    def print_to_console(self, forecast):
         print(self.location + '(latitude: ' + str(self.latitude) + ' longitude: ' + str(self.longitude) + ')\n')
 
         # build the date list
@@ -67,9 +70,54 @@ class WeatherForecast:
 
             array_index += 1
 
+    def print_to_html (self, forecast):
+        # build the date list
+        date_list = WeatherForecast.get_date_list(self)
+        array_index = 0
+
+        # create the top html file content
+        html_str = """
+        <!DOCTYPE html>
+        <html>
+            <head lang="en">
+            <meta charset="UTF-8">
+            <title>Weather Forecast</title>
+        </head>
+        <body>
+            <header>
+        """ + self.location + " (latitude: " + str(self.latitude) + " longitude: " + str(self.longitude) + ")" + """
+            </header>
+            <main>
+                <ol>
+        """
+
+        # build the forecast html
+        for day in forecast:
+            current_date = str(date_list[array_index]).split(' ')[0].split('-')
+
+            html_str += '<li>' + \
+                        WeatherForecast.WEEK[date_list[array_index].weekday()] + \
+                        ' - ' + WeatherForecast.MONTH[int(current_date[1]) - 1] + \
+                        ' ' + str(current_date[2]) + ', ' + str(current_date[0]) + \
+                        '<br>' + day['weather'][0]['description'] + '<br>' + \
+                        'High: ' + str(day['temp']['max']) + ' Low: ' + str(day['temp']['min']) + \
+                        '</li>'
+
+        # end of the html content
+        html_str += """
+                </ol>
+            </main>
+        </body>
+        </html>
+        """
+
+        html_file = open('forecast.html', 'w')
+        html_file.write(html_str)
+        html_file.close()
+
 #testing
-#location0 = WeatherForecast("London, UK")
-#print(location0.latitude)
+location0 = WeatherForecast("London, GB")
+location0.get_weather_forecast()
 #print(location0.longitude)
 #WeatherForecast("Ann Arbor, MI")
 #WeatherForecast("London") # still fails
