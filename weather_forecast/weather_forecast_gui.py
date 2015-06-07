347bcce2df860f84383be8e0e92038fcc2d663a8
sdaniels <daniels.scott.r@gmail.com>
2015-06-07 18:15:49 -0400

    changed the gui interface

--- weather_forecast/weather_forecast_gui.py
+++ weather_forecast/weather_forecast_gui.py
@@ -1,42 +1,41 @@
 __author__ = 'sdaniels'
 
 import tkinter
-from tkinter import ttk
 
 from weather_forecast import WeatherForecast
 
-class WeatherForecastGUI:
+class WeatherForecastGUI(tkinter.Tk):
     def __init__(self, parent):
+        tkinter.Tk.__init__(self, parent)
         self.parent = parent
-        self.createUI()
+        self.initialize()
 
     def close(self):
         exit(0)
 
-    def createUI(self):
-        the_content = ttk.Frame(root)
-        the_frame = ttk.Frame\
-            (the_content, borderwidth=5, relief="sunken", width=500, height=200)
+    def initialize(self):
+        self.grid()
 
-        location_label = ttk.Label(the_content, text="Enter location(s)")
-        self.the_location = ttk.Entry(the_content)
+        entry_label = tkinter.Label(self, anchor="w", text='Enter Location(s) (Example: Ann Arbor, MI; London; 48198 and etc.)')
+        entry_label.grid(column=0,row=1)
 
-        ok_button = ttk.Button(the_content, text="Get Forecast", command=self.getForecast)
-        cancel_button = ttk.Button(the_content, text="Cancel", command=self.close)
+        self.entry = tkinter.Entry(self)
+        self.entry.grid(column=0, columnspan=2, row=2,sticky='EW')
 
-        the_content.grid(column=0, row=0)
-        the_frame.grid(column=0, row=0, columnspan=10, rowspan=5)
-        location_label.grid(column=3, row=1, columnspan=2)
-        self.the_location.grid(column=3, row=2, columnspan=2)
-        ok_button.grid(column=3, row=4)
-        cancel_button.grid(column=4, row=4)
+        ok_button = tkinter.Button(self, text="Get Forecast", command=self.getForecast)
+        ok_button.grid(column=0,row=3)
+
+        cancel_button = tkinter.Button(self, text="Cancel", command=self.close)
+        cancel_button.grid(column=1,row=3)
+
+        self.grid_columnconfigure(0,weight=1)
 
 
     def getForecast(self):
-        a_location = WeatherForecast(self.the_location.get())
+        a_location = WeatherForecast(self.entry.get())
         a_location.get_weather_forecast()
 
 if __name__ == '__main__':
-    root =  tkinter.Tk()
-    app = WeatherForecastGUI(root)
-    root.mainloop()
+    app = WeatherForecastGUI(None)
+    app.title("Weather Forecaster")
+    app.mainloop()
