abc9169a70bfeefbd224c1cdf00818feb85e7dc1
sdaniels <daniels.scott.r@gmail.com>
2015-06-08 21:31:33 -0400

    Updated gui to allow user to select type of report to output

--- weather_forecast/weather_forecast_gui.py
+++ weather_forecast/weather_forecast_gui.py
@@ -22,11 +22,26 @@ class WeatherForecastGUI(tkinter.Tk):
         self.entry = tkinter.Entry(self)
         self.entry.grid(column=0, columnspan=2, row=2,sticky='EW')
 
+
+        # print_to_console/output_to_html_bool are BooleanVars initialized False
+        self.print_to_console_bool = tkinter.BooleanVar()
+        self.output_to_html_bool = tkinter.BooleanVar()
+        self.print_to_console_bool.set(False)
+        self.output_to_html_bool.set(False)
+
+        print_to_console_box = tkinter.Checkbutton \
+            (self, text="Print to Console", variable=self.print_to_console_bool,onvalue=1, offvalue=0)
+        output_to_html_box = tkinter.Checkbutton \
+            (self, text="Output to HTML", variable=self.output_to_html_bool,onvalue=1, offvalue=0)
+
+        print_to_console_box.grid(column=0, row=3)
+        output_to_html_box.grid(column=0, row=4)
+
         ok_button = tkinter.Button(self, text="Get Forecast", command=self.getForecast)
-        ok_button.grid(column=0,row=3)
+        ok_button.grid(column=0,row=6)
 
         cancel_button = tkinter.Button(self, text="Cancel", command=self.close)
-        cancel_button.grid(column=1,row=3)
+        cancel_button.grid(column=1,row=6)
 
         self.grid_columnconfigure(0,weight=1)
 
@@ -34,6 +49,16 @@ class WeatherForecastGUI(tkinter.Tk):
     def getForecast(self):
         a_location = WeatherForecast(self.entry.get())
         a_location.get_weather_forecast()
+
+        # only show the selected report output type
+        if self.print_to_console_bool.get():
+            a_location.print_to_console();
+
+        if self.output_to_html_bool.get():
+            a_location.output_to_html();
+
+        if (self.print_to_console_bool.get() == 0) and (self.output_to_html_bool.get() == 0):
+            print('No report selected!')
 
 if __name__ == '__main__':
     app = WeatherForecastGUI(None)
